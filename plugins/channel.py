6f5c249b2d862700ce7610fc269459eb37c60baa
amarsmartindia <personalinformation267@gmail.com>
2024-12-25 00:29:09 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -54,10 +54,10 @@ async def send_movie_updates(bot, file_name, file_id):
     if not poster_url or not caption:
         return
     btn = [
-        [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
+        [InlineKeyboardButton('Get File', url=f'@amarsmartindia_movieproviderbot?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
     ]
     reply_markup = InlineKeyboardMarkup(btn)
-    movie_update_channel =await db.movies_update_channel_id()
+    movie_update_channel =await db.movies_update_channel_i00t. (t.me/disscussiofilm/)
     try:
         await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, photo=poster_url, caption=caption, reply_markup=reply_markup)
     except Exception as e:
