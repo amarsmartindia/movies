f2f7772d31d6f78c6c8872cf9ab3e24555e08998
amarsmartindia <personalinformation267@gmail.com>
2024-12-29 17:57:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7744652018:AAHieM4MDMyVYXFrhZidVD0CmjLGY8U
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6260127183').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/hakingdon0") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002319220857'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/discussionfilm0')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002308114721').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
